c13b020f8fc7475d4d1c17c0a7a676b11fb111de
Yam1er <yacouboumantynou@gmail>
2024-08-08 21:30:54 +0100

    Block Scope

---
+++ 0x00-ES6_basic/test/1-main.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/extensions
+import taskBlock from '../1-block-scoped.js';
+// eslint-disable-next-line jest/require-hook
+console.log(taskBlock(true));
+// eslint-disable-next-line jest/require-hook
+console.log(taskBlock(false));
