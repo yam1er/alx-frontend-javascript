832c3330fa130d6b73c23e659ed8231a9d89fc97
Yam1er <yacouboumantynou@gmail>
2024-08-10 19:46:15 +0100

    Rest parameter syntax for functions

--- 0x00-ES6_basic/test/3-main.js
+++ 0x00-ES6_basic/test/3-main.js
@@ -1,3 +1,6 @@
+/* eslint-disable import/extensions */
+/* eslint-disable jest/require-hook */
+/* eslint-disable new-cap */
 import getSumOfHoods from '../3-default-parameter.js';
 
 console.log(getSumOfHoods(34));
