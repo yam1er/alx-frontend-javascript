4894d5f75b2ebd2ec001f8021d198748212aa3a2
Yam1er <yacouboumantynou@gmail>
2024-08-11 21:24:43 +0100

    10. For...of Loops

---
+++ 0x00-ES6_basic/test/10-main.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/extensions */
+/* eslint-disable jest/require-hook */
+/* eslint-disable new-cap */
+import appendToEachArrayValue from '../10-loops.js';
+
+console.log(appendToEachArrayValue(['appended', 'fixed', 'displayed'], 'correctly-'));
