78231f0b1f688f416c1ad76240052d7bcb90e91b
Yam1er <yacouboumantynou@gmail>
2024-08-11 12:41:49 +0100

    The wonders of spread syntax

--- 0x00-ES6_basic/test/4-main.js
+++ 0x00-ES6_basic/test/4-main.js
@@ -1,4 +1,7 @@
+/* eslint-disable import/extensions */
+/* eslint-disable jest/require-hook */
+/* eslint-disable new-cap */
 import returnHowManyArguments from '../4-rest-parameter.js';
 
-console.log(returnHowManyArguments("one"));
-console.log(returnHowManyArguments("one", "two", 3, "4th"));
+console.log(returnHowManyArguments('one'));
+console.log(returnHowManyArguments('one', 'two', 3, '4th'));
---
+++ 0x00-ES6_basic/test/5-main.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/extensions */
+/* eslint-disable jest/require-hook */
+/* eslint-disable new-cap */
+import concatArrays from '../5-spread-operator.js';
+
+console.log(concatArrays(['a', 'b'], ['c', 'd'], 'Hello'));
