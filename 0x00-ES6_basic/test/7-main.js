ce0f8a5979237566e7d966a5d97beaf6a673e427
Yam1er <yacouboumantynou@gmail>
2024-08-11 14:27:53 +0100

    Object property value shorthand syntax

---
+++ 0x00-ES6_basic/test/7-main.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/extensions */
+/* eslint-disable jest/require-hook */
+/* eslint-disable new-cap */
+import getBudgetObject from '../7-getBudgetObject.js';
+
+console.log(getBudgetObject(400, 700, 900));
