60e7c1921d14750b43d75dedeb4eea2ce8132a72
Yam1er <yacouboumantynou@gmail>
2024-08-11 13:58:49 +0100

    Take advantage of template literals

---
+++ 0x00-ES6_basic/test/6-main.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/extensions */
+/* eslint-disable jest/require-hook */
+/* eslint-disable new-cap */
+import getSanFranciscoDescription from '../6-string-interpolation.js';
+
+console.log(getSanFranciscoDescription());
