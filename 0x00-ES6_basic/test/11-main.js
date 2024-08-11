95a745630a632567df4f6fefdda34208dfd1e5bd
Yam1er <yacouboumantynou@gmail>
2024-08-11 21:33:31 +0100

    Iterator

---
+++ 0x00-ES6_basic/test/11-main.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/extensions */
+/* eslint-disable jest/require-hook */
+/* eslint-disable new-cap */
+import createEmployeesObject from '../11-createEmployeesObject.js';
+
+console.log(createEmployeesObject('Software', ['Bob', 'Sylvie']));
