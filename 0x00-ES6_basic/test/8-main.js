0c06a21d7e75ef1ee3245292579f15ccd9022c6b
Yam1er <yacouboumantynou@gmail>
2024-08-11 20:26:30 +0100

    No need to create empty objects before adding in properties

---
+++ 0x00-ES6_basic/test/8-main.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/extensions */
+/* eslint-disable jest/require-hook */
+/* eslint-disable new-cap */
+import getBudgetForCurrentYear from '../8-getBudgetCurrentYear.js';
+
+console.log(getBudgetForCurrentYear(2100, 5200, 1090));
