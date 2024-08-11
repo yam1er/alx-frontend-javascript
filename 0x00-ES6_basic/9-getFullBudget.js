fe38a925a004fe2e1e63d4cc3c619a280debd435
Yam1er <yacouboumantynou@gmail>
2024-08-11 20:45:33 +0100

    Fixed

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -8,12 +8,8 @@ export default function getFullBudgetObject(income, gdp, capita) {
 
   const fullBudget = {
     ...budget,
-    getIncomeInDollars(income) {
-      return `$${income}`;
-    },
-    getIncomeInEuros(income) {
-      return `${income} euros`;
-    },
+    getIncomeInDollars: (income) => `$${income}`,
+    getIncomeInEuros: (income) => `${income} euros`,
   };
 
   return fullBudget;
