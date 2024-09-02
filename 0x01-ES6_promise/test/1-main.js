c7c97f12666adccbdf5b1eba0360bd4949ec7e8a
Yam1er <yacouboumantynou@gmail>
2024-09-02 20:55:30 +0100

    Don't make a promise...if you know you can't keep it

---
+++ 0x01-ES6_promise/test/1-main.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/extensions */
+import getFullResponseFromAPI from '../1-promise.js';
+
+console.log(getFullResponseFromAPI(true));
+console.log(getFullResponseFromAPI(false));
