788ce2f28d1305532917deb87fb0e025d1b305c5
Yam1er <yacouboumantynou@gmail>
2024-09-02 20:42:36 +0100

    Common.js fixed

--- 0x01-ES6_promise/test/0-main.js
+++ 0x01-ES6_promise/test/0-main.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/extensions */
-import getResponseFromAPI from './0-promise.js';
+import getResponseFromAPI from '../0-promise.js';
 
 const response = getResponseFromAPI();
 console.log(response instanceof Promise);
