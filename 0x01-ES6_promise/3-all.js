96e74da88c7e8381115c01663eb9fe0ae22c84aa
Yam1er <yacouboumantynou@gmail>
2024-09-03 10:08:08 +0100

    Handle multiple successful promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,18 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+import { uploadPhoto, createUser } from '../utils.js';
+
+export default function handleProfileSignup() {
+  const p1 = uploadPhoto();
+  const p2 = createUser();
+
+  return Promise.all([p1, p2])
+    .then((value) => {
+      console.log(
+        `${value[0].body} ${value[1].firstName} ${value[1].lastName}`,
+      );
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
