9cc77b4c2062f6abe4b99cbbe49f52ae850553b9
Yam1er <yacouboumantynou@gmail>
2024-09-04 15:50:30 +0100

    fix

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,8 +1,8 @@
 export default function guardrail(mathFunction) {
   const queue = [];
   try {
-  const result = mathFunction();
-  queue.push(result);
+    const result = mathFunction();
+    queue.push(result);
   } catch (error) {
     queue.push(`Error: ${error.message}`);
   } finally {
